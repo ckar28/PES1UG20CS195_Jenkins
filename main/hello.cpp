ec84a6666e12b4f7747e59b52278f811be4b4266
Karthik Gupta <90790015+ckar28@users.noreply.github.com>
2023-02-17 23:11:52 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "Karthik Gupta(PES1UG20CS195) have successfully built and run\n";
+  cout << "Karthik Gupta(PES1UG20CS195) has successfully built and run\n";
   
   return 0;
 }
